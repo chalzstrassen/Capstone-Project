8dfd6d0affdbbcbfe5686989f60f0ba7341fe47e
chalzstrassen <yuchao.137chen@gmail.com>
2015-05-12 21:41:36 -0400

    Start generating controller routes for API.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#root'
+
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+
+  namespace :api, defaults: { format: :json } do
+  	resources :books
+  	resources :collections
+  end
 end
