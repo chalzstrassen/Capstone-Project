df42545b9d67e26eb37a17324f6cc8048db1a5fc
App Academy Student <appacademy@Apps-Mac-mini-10.local>
2015-05-13 15:31:08 -0400

    Complete all controller actions for Books and Collections.

--- app/controllers/api/collections_controller.rb
+++ app/controllers/api/collections_controller.rb
@@ -11,15 +11,35 @@ module Api
     end
 
     def create
+      @collection = current_user.collections.new(collection_params)
 
+      if @collection.save
+        render json: @collection
+      else
+        render json: @collection.errors.full_messages, status: 422
+      end
     end
 
     def destroy
-
+      @collection = Collection.find(params[:id])
+      @collection.destroy
+      render json: {}
     end
 
     def update
-
+      @collection = current_user.collections.find(params[:id])
+      if @collection.update_attributes(collection_params)
+        render json: @collection
+      else
+        render json: @collection.errors.full_messages, status: 422
+      end
     end
+
+    private
+
+      def collection_params
+        params.require(:collection).permit(:name, :description)
+      end
+
   end
 end
