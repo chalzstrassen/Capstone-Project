066c8538dcac888119de19369bbb593ef54d9e26
chalzstrassen <yuchao.137chen@gmail.com>
2015-05-13 21:49:31 -0400

    Begin initializing router actions and now need to create a composite view.

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -3,10 +3,25 @@ Enwritt.Routers.Router = Backbone.Router.extend({
     this.$rootEl = options.$rootEl
   },
   routes: {
-    '': 'showLibraryRegions'
+    '': 'libraryItems'
   },
   libraryItems: function () {
-    
+    var collCollection = new Enwritt.Collections.Collections();
+    var bookCollection = new Enwritt.Collections.Books();
+    collCollection.fetch({
+    	success: function () {
+    		bookCollection.fetch({
+    			success: function () {
+    				var libView = new Enwritt.Views.LibraryView({
+    					collCollection: collCollection,
+    					bookCollection: bookCollection
+    				});
+
+    				this.$rootEl.html(libView.render().$el);
+    			}
+    		}
+    	}
+    });
   }
 
 });
