5e0aa49a20e04ba9de752a0eca969c85ddc40ba8
chalzstrassen <yuchao.137chen@gmail.com>
2015-05-18 23:12:51 -0400

    Create book form to publish books, need to call controller action.

--- app/assets/javascripts/views/books/book_form.js
+++ app/assets/javascripts/views/books/book_form.js
@@ -1,8 +1,11 @@
 Enwritt.Views.BookForm = Backbone.ModalView.extend({
   template: JST["books/form"],
+  tagName: "form",
+  className: "book-form",
   render: function () {
-    var content = this.template({
-      book: this.model
-    });
+    var content = this.template({ book: this.model });
+    this.$el.html(content);
+
+    return this;
   }
 });
