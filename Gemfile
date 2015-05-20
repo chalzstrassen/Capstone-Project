8648ef29a15e185b12cdb16d6424285976552355
chalzstrassen <yuchao.137chen@gmail.com>
2015-05-20 15:06:49 -0400

    The pdf of the book now has a First page thumbnail.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'figaro'
 gem 'aws-sdk', '< 2.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'rmagick'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,13 @@ class Book < ActiveRecord::Base
   validates :author_id, :title, :genre, :synopsis, presence: true
   has_attached_file :cover, default_url: "default.png"
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
-  has_attached_file :content, styles: {thumbnail: "60x60#"}, default_url: "/"
+  has_attached_file :content, styles: { thumbnail: ["100x100#", :png] }, default_url: "/"
   validates_attachment :content, content_type: { content_type: "application/pdf" }
+
   belongs_to(:author,
             class_name: "User",
             foreign_key: :author_id,
             primary_key: :id,
             )
+
 end
