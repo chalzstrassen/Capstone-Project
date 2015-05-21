74173837ed30048d82a57dbb5326ad927961c964
chalzstrassen <yuchao.137chen@gmail.com>
2015-05-21 13:58:12 -0400

    Complete general, no filter search for anyone.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,3 +1,14 @@
 class SearchesController < ApplicationController
+	def index
+		@search_results = Book.search_on_title_synopsis(params[:query])
+							  .page(params[:page])
+		render :index
+	end
 
+	def filter_by_genre
+		genre_filtered = Book.where(genre: params[:filter][:genres])
+		@search_results = genre_filtered.search_on_title_synopsis(params[:query])
+										.page(params[:page])
+		render :index
+	end
 end
